c57225f2faabe535fab24dcb2df114ac0d78317a
wamzybasu <wamikabasu786@gmail.com>
2022-08-23 18:55:13 +0530

    WB: Created Home Views and the HomeController

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
