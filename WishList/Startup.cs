9e285ef4f9c82fa3d52658ba5fbf6be52be6822c
wamzybasu <wamikabasu786@gmail.com>
2022-08-23 18:47:33 +0530

    WB: Added UseRouting in Configure. Added UseEndpoints in Configure.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -30,7 +30,8 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseRouting().UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
