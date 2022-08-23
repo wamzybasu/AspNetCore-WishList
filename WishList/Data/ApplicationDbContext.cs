f7fc4d6c61a6799f13326895eadf5275ae5fc4ee
wamzybasu <wamikabasu786@gmail.com>
2022-08-23 19:02:11 +0530

    WB: Created Item Model With EntityFramework Support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
